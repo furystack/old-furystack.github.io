d389d9852e38ba29501449ad5adbf1cfea0dfdc0
Gallay Lajos <gallay.lajos@gmail.com>
2019-04-27 21:13:31 +0200

    feat(home):cards

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,15 @@
 import React, { useContext } from "react";
 import Typography from "@material-ui/core/Typography";
 import { ThemeContext } from "../context/theme-context";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import gettingStarted from "../images/getting-started.jpg";
+import packages from "../images/packages.jpg";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 export const Home: React.FunctionComponent = () => {
   const theme = useContext(ThemeContext);
@@ -8,13 +17,88 @@ export const Home: React.FunctionComponent = () => {
     <div>
       <Typography
         variant="subtitle1"
-        style={{ color: theme.palette.text.primary }}
+        style={{ color: theme.palette.text.primary, textAlign: "center" }}
       >
         FuryStack is a flexible framework that allows you to build complex
-        services fast and easily.
+        backend services fast and easily.
       </Typography>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap"
+        }}
+      >
+        <Card style={{ width: 350, margin: "1em 2em" }}>
+          <Link
+            to="/packages"
+            style={{ color: "initial", textDecoration: "initial" }}
+          >
+            <CardActionArea>
+              <CardMedia
+                style={{ height: 220, minWidth: 350 }}
+                component="img"
+                image={packages}
+                title="Getting Started"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Packages
+                </Typography>
+                <Typography component="p">
+                  Check out the NPM package of each layer of fury. Even the
+                  dachshund will be enraged.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Link>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+        <Card style={{ width: 350, margin: "1em 2em" }}>
+          <CardActionArea
+            onClick={() =>
+              window.open(
+                "https://github.com/furystack/furystack/tree/master/examples/repository",
+                "_blank"
+              )
+            }
+          >
+            <CardMedia
+              style={{ height: 220, minWidth: 350 }}
+              component="img"
+              image={gettingStarted}
+              title="Getting Started"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Getting started
+              </Typography>
+              <Typography component="p">
+                Check out the example repository. More detailed onboarding docs
+                coming soon. Honestly.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
       <Typography
-        variant="body1"
+        variant="subtitle1"
         style={{ color: theme.palette.text.secondary }}
       >
         <ul>
