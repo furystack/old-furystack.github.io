dc9183e35fa3a5fe25950c5b9b99273750d614e7
Gallay Lajos <gallay.lajos@gmail.com>
2019-05-03 14:55:16 +0200

    fix(app):ThemeContext&MuiThemeProvider

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,57 +2,65 @@ import * as React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MainAppBar } from "./main-app-bar";
 import { ThemeContext } from "../context/theme-context";
-import { useContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Home } from "../pages/home";
 import { GettingStarted } from "../pages/getting-started";
 import { Guide } from "../pages/guide";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 export const App: React.FunctionComponent = () => {
-  const theme = useContext(ThemeContext);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: theme.palette.background.default
-      }}
-    >
-      <BrowserRouter>
-        <CssBaseline />
-        <MainAppBar />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            overflow: "hidden"
-          }}
-        >
+    <ThemeContext.Consumer>
+      {theme => (
+        <MuiThemeProvider theme={theme}>
           <div
             style={{
-              margin: "1em",
-              overflow: "auto"
+              display: "flex",
+              flexDirection: "column",
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              background: theme.palette.background.default
             }}
           >
-            <Switch>
-              <Route path="/getting-started">
-                <GettingStarted />
-              </Route>
-              <Route path="/guide">
-                <Guide />
-              </Route>
-              <Route>
-                <Home />
-              </Route>
-            </Switch>
+            <BrowserRouter>
+              <CssBaseline />
+              <MainAppBar />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  overflow: "hidden"
+                }}
+              >
+                <div
+                  style={{
+                    margin: "1em",
+                    overflow: "auto",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center"
+                  }}
+                >
+                  <Switch>
+                    <Route path="/getting-started">
+                      <GettingStarted />
+                    </Route>
+                    <Route path="/guide">
+                      <Guide />
+                    </Route>
+                    <Route>
+                      <Home />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </BrowserRouter>
           </div>
-        </div>
-      </BrowserRouter>
-    </div>
+        </MuiThemeProvider>
+      )}
+    </ThemeContext.Consumer>
   );
 };
